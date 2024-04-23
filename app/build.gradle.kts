75176b48e576ab160dde0ef877fc44bd2b53ff2e
Marek Defeciński <marek.defecinski@epassi.com>
2024-04-23 23:12:51 +0200

    Added logback binding to slf4j

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+
     implementation(libs.logging.slf4j)
+    implementation(libs.logging.logback)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
